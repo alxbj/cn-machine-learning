eb71ac1ee2f0fdafd94ec0a047cfe872a835ce1e
alxbj <44975835+alxbj@users.noreply.github.com>
2018-11-23 08:49:21 +0800

    1st commit boston_housing.py

---
+++ boston_housing/Boston_Housing_By_Alxbj.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 22:50:28 2018
+
+@author:  7961159@qq.com 李欣
+"""
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import ShuffleSplit
+
+#import visuals as vs
+ 
+# load the Boston housing dataset
+
+data = pd.read_csv('housing.csv')
+#print(data)
+prices = data['MEDV']
+features = data.drop('MEDV',axis = 1)
+
+print(data.head())
+
